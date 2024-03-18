eb2c1160ff18060c17114d118e38ac5e95c88a5c
Aldrei <aldreidosantos@gmail.com>
2024-03-18 10:49:52 +0000

    feat: add environment variables

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+export interface ProcessEnv {
+  DB_SERVICE: string
+  DB_USER: string
+  DB_PASS: string
+  DB_HOST: string
+  DB_PORT: string
+}
+
+dotenv.config()
+const ENV = process.env as unknown as ProcessEnv
+
+export default ENV
--- src/database/instance/index.ts
+++ src/database/instance/index.ts
@@ -1,13 +1,15 @@
+import ENV from "@/config";
 import { Sequelize } from "sequelize";
 
 /**
  * Create a Docker named network to link with Postgres.
 */
-const db = new Sequelize('mysql://root@172.17.0.2:3306/IMOB')
+const db = new Sequelize(`
+  ${ENV.DB_SERVICE}://${ENV.DB_USER}@${ENV.DB_HOST}:${ENV.DB_PORT}`
+)
 
 try {
-  const init = async () => await db.authenticate();
-  init()
+  (async () => await db.authenticate())();
   
   console.log('Connection has been established successfully.');
 } catch (error) {
