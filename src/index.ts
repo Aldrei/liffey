965110810277567e1835434ecfe2d1dc7a214ab9
Aldrei <aldreidosantos@gmail.com>
2024-03-21 23:24:51 +0000

    feat: add morgan to track logs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import morgan from 'morgan';
+
+import fs from 'fs';
 
 /** GraphQL */
 import { resolvers } from '@/schemas/resolvers';
@@ -16,6 +20,7 @@ import * as userRoutes from '@/routes/user.route';
 /** Check Environment */
 import ENV from '@/config';
 import { isDev } from '@/helpers';
+import path from 'path';
 
 (globalThis as any).__DEV__ = isDev();
 
@@ -27,6 +32,17 @@ interface MyContext {
 }
 
 const app = express();
+
+/**
+ * Write logs.
+*/
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/dev.log'), { flags: 'a' })
+
+accessLogStream.on('error', (err) => {
+  console.error('### LOG STREAM ERROR: Error writing to dev.log:', err);
+})
+
+app.use(morgan('common', { stream: accessLogStream }))
 
 const httpServer = http.createServer(app);
 const server = new ApolloServer<MyContext>({
@@ -62,3 +78,4 @@ listen()
 
 console.log(`Running in ${ENV.NODE_ENV} mode(globalThis.__DEV__: ${globalThis.__DEV__})`);
 console.log(`🚀 Server ready at http://localhost:${ENV.APP_PORT}/graphql`);
+
