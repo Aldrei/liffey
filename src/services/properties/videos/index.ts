88316aaabd4faa98ce0f93f04f4b182d3f2e6ec4
Aldrei <aldreidosantos@gmail.com>
2024-04-07 01:10:48 +0100

    feat: implement DELETE property video

--- src/services/properties/videos/index.ts
+++ src/services/properties/videos/index.ts
@@ -1,4 +1,7 @@
 import { Videos } from "@/database/models";
+import { SYSTEM_VIDEO_STORAGE_PATH } from "@/helpers/config";
+
+import fs from 'fs';
 
 import { Request, Response } from 'express';
 
@@ -16,5 +19,26 @@ export const store = async (req: Request, res: Response): Promise<any> => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: error.message });
+  }
+}
+
+export const destroy = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { property_id, video_id } = req.params
+
+    const video = await Videos.findOne({ where: { id: Number(video_id), property_id: Number(property_id) } })
+
+    const videoPath = `${SYSTEM_VIDEO_STORAGE_PATH}${video.src}`
+
+    fs.unlink(videoPath, error => {
+      if (error) throw Error(`Error deleting video. ${error.message}`)
+    })
+
+    await Videos.destroy({ where: { id: video_id } })
+
+    return res.status(200).json({ message: 'Video deleted successfully.' })
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message })
   }
 }
