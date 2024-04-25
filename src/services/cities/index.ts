ff9e57c6bbd81f97d826924b0ecd58187e403f6b
Aldrei <aldreidosantos@gmail.com>
2024-04-25 22:17:57 +0100

    feat: implement cities list service

--- src/services/cities/index.ts
+++ src/services/cities/index.ts
@@ -1,6 +1,7 @@
-import { Cities, ICity } from '@/database/models'
+import { Cities, Clients, ICity } from '@/database/models'
 import { cityParseEnToPt } from '@/database/parse/city'
 import { transformCity } from '@/database/transformers/city'
+import { getPaginateConditions, getPaginateMetadata } from '@/helpers/paginate'
 import { extractUserFromToken } from '@/helpers/token'
 import { Request, Response } from 'express'
 import { Op } from 'sequelize'
@@ -33,6 +34,54 @@ export const search = async (req: Request, res: Response): Promise<any> => {
     // Translated fields
     if (lang !== 'EN') {
       enDataFields.data = transformedCities.map((item: ICity) => cityParseEnToPt<TCityResponse>(item))
+    }
+
+    return res.status(200).json(enDataFields);
+  } catch (error) {
+    console.error( error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+export const list = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { lang, page, orderASC } = req.query
+    const { client: clientJwt } = extractUserFromToken(req)
+
+    const client = await Clients.findOne({ where: { id: clientJwt.id } })
+
+    // Raw data
+    const total = await Cities.count({ 
+      where: {
+        client_id: client.id
+      },
+    })
+
+    const dataList = await Cities.findAll({
+      where: {
+        client_id: client.id,
+      },
+      order: [
+        ['id', orderASC === 'false' ? 'DESC' : 'ASC']
+      ],
+      ...getPaginateConditions(page)
+    })
+
+    // Transformed data
+    const transformedData = dataList.map((item: ICity) => transformCity(item))
+
+    const enDataFields = {
+      paginate: {
+        data: transformedData,
+        message: 'Success',
+        status: 200,
+        ...getPaginateMetadata(page, total, 'cities'),
+      }
+    }
+
+    // Translated fields
+    if (lang !== 'EN') {
+      enDataFields.paginate.data = transformedData.map((item: ICity) => cityParseEnToPt(item))
     }
 
     return res.status(200).json(enDataFields);
