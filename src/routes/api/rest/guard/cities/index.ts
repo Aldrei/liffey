b96bb2a7131be1dd89e0fddcfec894e7d47e524c
Aldrei <aldreidosantos@gmail.com>
2024-04-25 22:19:05 +0100

    feat: implement cities list service

--- src/routes/api/rest/guard/cities/index.ts
+++ src/routes/api/rest/guard/cities/index.ts
@@ -1,6 +1,7 @@
 import { router } from "@/express.instance";
-import { search } from "@/services/cities";
+import { list, search } from "@/services/cities";
 
 router.get('/api/cities/search/:search', search)
+router.get('/api/cities', list)
 
 export default router
