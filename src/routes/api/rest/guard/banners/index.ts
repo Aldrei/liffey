ff2b39a1e5b9d274d5ea94f86619c5c22d89e7a6
Aldrei <aldreidosantos@gmail.com>
2024-04-29 10:53:29 +0100

    feat: implement banner delete service

--- src/routes/api/rest/guard/banners/index.ts
+++ src/routes/api/rest/guard/banners/index.ts
@@ -1,8 +1,9 @@
 import { router } from "@/express.instance";
-import { detail, list, update } from "@/services/banners";
+import { destroy, detail, list, update } from "@/services/banners";
 
 router.get('/api/banners', list)
 router.get('/api/banners/:id', detail)
 router.put('/api/banners/:id', update)
+router.delete('/api/banners/:id', destroy)
 
 export default router
